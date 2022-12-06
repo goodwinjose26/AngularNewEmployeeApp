e85610578b676db164c795b83faf85e6ac4fb50d
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-06 21:01:00 +0530

    service

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+
+  empcode=""
+  name=""
+  designation=""
+  salary=""
+  cmpname=""
+  mobno=""
+  username=""
+  password=""
+
+  constructor(private api:ApiService) {}
+
+  addemployee=()=>
+  {
+    let data:any={"empcode":this.empcode,"name":this.name,"designation":this.designation,"salary":this.salary,"cmpname":this.cmpname,"mobno":this.mobno,"username":this.username,"password":this.password}
+    console.log(data)
+    this.api.addemployee(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+      }
+    )
+  }
 
 }
